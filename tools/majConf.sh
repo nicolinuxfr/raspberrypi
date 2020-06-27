02d33a96c250c35505114f4870a46ac36b56e5b7
Nicolas <nicolas@mgig.fr>
2020-06-27 10:59:00 +0200

    Ajout majConf

---
+++ tools/majConf.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Script qui permet de mettre à jour la configuration depuis le dépôt et d'installer les fichiers au bon endroit.
+
+cd /home/pi/config
+git pull origin
+
+# Installation de la nouvelle version du fichier Caddyfile
+cp -rf etc/caddy/Caddyfile /etc/caddy/
+chown caddy:caddy /etc/caddy/Caddyfile
+chmod 444 /etc/caddy/Caddyfile
+
+# Rechargements en cas de changement
+systemctl reload caddy
